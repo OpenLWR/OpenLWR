5f9ba7103c8f1f22bfccf4698facb0ca3ff4afea
Roger van Bommel <fjvanbommel@gmail.com>
2024-04-25 20:39:19 +0200

    dont send position if player did not move

--- Assets/Scripts/globals.gd
+++ Assets/Scripts/globals.gd
@@ -3,5 +3,8 @@ extends Node
 var server_ip_requested_tojoin = null
 var username_requested_tojoin = null
 var connection_state = WebSocketPeer.STATE_CLOSED
+var last_sent_x = 0
+var last_sent_y = 0
+var last_sent_z = 0
 
 var renderer = null

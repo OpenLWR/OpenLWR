404f74ca4a6a35b10c7bb9c65ea6c3170c4d4865
kcinnaJlol <39594309+kcinnaJlol@users.noreply.github.com>
2024-04-30 09:53:54 +0200

    improve lobby screen
    - the text fields are now a single line (and don't show a scroll bar)
    - you can tab between the fields/dropdown menu/button
    - the 'Control Room' scene is selected by default
    - the server ip field starts with 'localhost' filled in
    - pressing enter while editing the username field will try to join the server

--- LobbyScreen.gd
+++ LobbyScreen.gd
@@ -1,23 +1,15 @@
-extends Node2D
+extends Control
 
-func _ready():
-	set_scene_dropdown(get_node("Scene"))
-
-func set_scene_dropdown(dropdown):
-	dropdown.add_item("Dev Test")
-	dropdown.add_item("Control Room")
-
-func connect_button_pressed():
+func _on_connect_button_pressed():
 	print("Connect")
-	var server_ip_requested = get_node("ServerIP/TextEdit").text
-	var username_requested = get_node("Username/TextEdit").text
-	if server_ip_requested == "":
-		server_ip_requested = "127.0.0.1:7001"
+	var server_ip_requested = $Panel/ServerIP/TextEdit.text
+	var username_requested = $Panel/Username/TextEdit.text
 	print(server_ip_requested)
 	print(username_requested)
 	globals.server_ip_requested_tojoin = server_ip_requested
 	globals.username_requested_tojoin = username_requested
-	if get_node("Scene").selected == 0:
-		get_tree().change_scene_to_file("res://node_3d.tscn")
-	else:
-		get_tree().change_scene_to_file("res://control_room_nmp2.tscn")
+	match $Panel/Scene.selected:
+		0:
+			get_tree().change_scene_to_file("res://node_3d.tscn")
+		1:
+			get_tree().change_scene_to_file("res://control_room_nmp2.tscn")

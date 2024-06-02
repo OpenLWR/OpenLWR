de2d46105aebcc485b9bbe0d25ccc34f2a63f6e1
watchful5406 <seantyler525@gmail.com>
2024-06-01 18:17:06 -0700

    Add resolution settings

--- Assets/Scenes/Player/graphics_menu.gd
+++ Assets/Scenes/Player/graphics_menu.gd
@@ -1,15 +1,29 @@
 extends Control
 
+var Resolutions = {
+	"2560x1440":Vector2(2560,1440),
+	"1920x1080":Vector2(1920,1080),
+	"1536x864":Vector2(1536,864),
+	"1366x768":Vector2(1366,768),
+	"1280x720":Vector2(1280,720),
+}
+
+@onready var viewport = get_viewport()
 
 func _ready():
 	set_occlusion_dropdown(get_node("Occlusion"))
 	set_antialiasing_dropdown(get_node("Antialiasing"))
 	set_vsync_dropdown(get_node("Vsync"))
-	var viewport = get_viewport()
+	set_resolution_dropdown(get_node("Resolution"))
 	viewport.msaa_3d = 3
 	viewport.use_taa = false
 	viewport.use_occlusion_culling = true
+	viewport.scaling_3d_mode = Viewport.SCALING_3D_MODE_BILINEAR
 	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	
+func set_resolution_dropdown(dropdown):
+	for r in Resolutions:
+		dropdown.add_item(r)
 	
 #TODO: actually set graphics quality
 func set_occlusion_dropdown(dropdown):
@@ -54,3 +68,10 @@ func _new_vsync_selected(index):
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
 	else:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		
+func new_resolution_selected(index):
+	var size = Resolutions.get(get_node("Resolution").get_item_text(index))
+	#compares the size to 1920x1080
+	var scale_factor = size[0]/1920
+	viewport.scaling_3d_scale = scale_factor
+	#DisplayServer.window_set_size(size)

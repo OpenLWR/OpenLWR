7a679ed199c7ea4783eb8ea078cbf7b068a8120d
kcinnaJlol <39594309+kcinnaJlol@users.noreply.github.com>
2024-06-11 20:59:36 +0200

    improve chat
    add chat fading, max amount of chat messages (currently 200) and scroll to a new chat message when one arrives

--- Assets/Scenes/Player/Chat.gd
+++ Assets/Scenes/Player/Chat.gd
@@ -1,5 +1,7 @@
 extends VBoxContainer
 
+const MAX_CHAT_MESSAGES = 200
+
 func _ready():
 	$/root/Node3D.connect(&"chat_message", _on_chat_message)
 	pass
@@ -8,6 +10,22 @@ func _on_chat_message(message: String):
 	var node = Label.new()
 	node.text = message
 	$Messages/VBoxContainer.add_child(node)
+	var child_count = $Messages/VBoxContainer.get_child_count()
+	if child_count > MAX_CHAT_MESSAGES:
+		var del = $Messages/VBoxContainer.get_child(0)
+		$Messages/VBoxContainer.remove_child(del)
+		del.queue_free()
+	await get_tree().process_frame
+	#HACK: this seems like the only way to scroll to the most recent chat
+	# message in the same frame
+	$Messages.scroll_vertical = 21 * child_count + 1
+	$AnimationPlayer.seek(0)
+	$AnimationPlayer.play($AnimationPlayer.assigned_animation)
+	pass
+
+func _on_mouse_enter_or_leave(entered: bool):
+	$AnimationPlayer.seek(0)
+	$AnimationPlayer.play("opaque" if entered else "fade")
 	pass
 
 func _on_text_edit_text_submitted(text):

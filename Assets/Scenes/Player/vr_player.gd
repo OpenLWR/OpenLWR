9f8796512ad7cd93fadc0ddca54d1f8b1d979802
watchful5406 <seantyler525@gmail.com>
2024-10-25 21:45:45 -0700

    VR stuff

--- Assets/Scenes/Player/vr_player.gd
+++ Assets/Scenes/Player/vr_player.gd
@@ -19,16 +19,20 @@ func _physics_process(_delta):
 	direction.z = direction.z * max_speed_z
 	#TODO: turn based on controller/player head orientation (selectable)
 	if direction:
-		var tween = get_tree().create_tween()
-		tween.set_parallel()
-		tween.tween_property(self, "rotation", Vector3(0,$XROrigin3D/XRCamera3D.rotation.y + deg_to_rad(90), 0), 0.2)
-		#self.rotation.y = 
-		# $XROrigin3D/RightController.rotation.y + deg_to_rad(90)
+		
+		
+		
 		self.translate_object_local(direction)
 
 func left_controller_joystick_input(name,value):
 	if name == "primary": 
 		joystick_left = value
+		
+	if name == "trigger" and value == 1:
+		var touching = $XROrigin3D/LeftController/Hand/Area3D.get_overlapping_areas()
+		
+		if $XROrigin3D/LeftController/Hand/Area3D.has_overlapping_areas():
+			touching[0].get_parent().switch_vr_player(touching[0].name)
 		
 func vr_controller_input(name,value):
 	if name == "trigger" and value == 1:
